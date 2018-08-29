9af07621b2a1f2ad54a55a14346c805e22a0a0a7
Bileonaire <leonkioko@Leons-MacBook-Pro.local>
2018-08-29 11:10:41 +0300

    [Chore #159965288] Add tests for user login

---
+++ authors/apps/authentication/tests/test_login.py
@@ -0,0 +1,48 @@
+""" module to test login. """
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from authors.apps.authentication.models import User
+
+
+class AuthenticationTests(APITestCase):
+    def setUp(self):
+        """ Setup data for the tests """
+        self.user1 = {
+            "username": "createduser1",
+            "email": "createduser1@user.user",
+            "password": "user123user"
+        }
+        self.reg_url = reverse('authentication:register')
+        self.login_url = reverse('authentication:login')
+        reg = self.client.post(self.reg_url, self.user1, format='json')
+
+    def test_successful_login_user(self):
+        """ Test that a user successfully logs in """
+        response = self.client.post(self.login_url, self.user1, format='json')
+        self.assertEqual(login_response.status_code, status.HTTP_201_CREATED)
+
+    def test_unsuccessful_login_with_wrong_email(self):
+        """ Test that a user cannot log in with a wrong email """
+        self.user1['email'] = "wrong@user.user"
+        response = self.client.post(self.login_url, self.user1, format='json')
+        self.assertEqual(login_response.status_code,
+                         status.HTTP_400_BAD_REQUEST)
+
+    def test_unsuccessful_login_with_wrong_password(self):
+        """ Test unsuccessful log in with a wrong email """
+        self.user1['password'] = "wrongpassword"
+        response = self.client.post(self.login_url, self.user1, format='json')
+        self.assertEqual(login_response.status_code,
+                         status.HTTP_400_BAD_REQUEST)
+
+    def test_unsuccessful_not_registered_user_login(self):
+        """ Test unsuccessful login for unregistered user. """
+        response = self.client.post(
+            self.login_url, {
+                "email": "unregistered@unreg.unreg",
+                "password": "unregistered"
+            }, format='json')
+        self.assertEqual(login_response.status_code,
+                         status.HTTP_400_BAD_REQUEST)
