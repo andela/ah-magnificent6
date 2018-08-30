0044a9ad07f0ef373132444cd8da8614b519a69d
Bileonaire <leonkioko@Leons-MacBook-Pro.local>
2018-08-30 17:15:47 +0300

    [Feature #159965298] Add token generation

---
+++ authors/apps/authentication/token.py
@@ -0,0 +1,16 @@
+import jwt
+import datetime
+
+from django.conf import settings
+
+
+def generate_jwt_token(email, username):
+    """Method to generate user jwt token."""
+    time = datetime.datetime.utcnow() + datetime.timedelta(seconds=86400)
+    token = jwt.encode({
+        "username": username,
+        "email": email,
+        "exp": time,
+    }, settings.SECRET_KEY, algorithm='HS256')
+
+    return token.decode('utf-8')
