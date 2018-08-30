0c99efd51c5597838a863252295c001e4bd9e1b4
Michael Nthiwa <mike.nthiwa@gmail.com>
2018-08-30 08:49:30 +0300

    #159965295 Configure Database and set up environment variable (#5)
    What does this PR do?
    
    Create an environment setting and configure AuthorsHaven app to use PostgreSQL as DB Engine.
    Description of Task to be completed?
    
    Using Twelve-factor methodology to configure the application with environment variables.
    Install Psycopg2-binary.
    Background context :
    
    Changing database default settings to read from .env file:
    .env sample file : sample .env file
    DATABASES = { 'default': env.db() }
    
    Relevant Pivotal Tracker story.
    * 159965295

---
+++ .env-sample
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# sample file for env
+
+DEBUG=on
+SECRET_KEY="secret key"
+DATABASE_URL=psql://username:password@127.0.0.1:5432/databasename
--- authors/settings.py
+++ authors/settings.py
@@ -11,18 +11,24 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import environ
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+env = environ.Env(DEBUG=(bool, False), )  # set default values and casting
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7pgozr2jn7zs_o%i8id6=rddie!*0f0qy3$oy$(8231i^4*@u3'
+
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -80,10 +86,7 @@ WSGI_APPLICATION = 'authors.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': env.db()
 }
 
 # Password validation
@@ -109,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
