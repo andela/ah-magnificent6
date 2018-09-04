2c3950eac89af35c4e8ad7d6c3db2dc0248b8d93
mbuvi <meshack.mbuvi@andela.com>
2018-09-04 15:27:34 +0300

    #160246870 Bg fix key error on get endpoint (#13)
    **What does this PR do?**
    - Fixes bug when a client sends a get request to signup and login endpoint.
    
    **Description of tasks to be completed**
    - Define methods on *signup* and *login* endpoints to act on *get* requests from clients.
    - Move JWT generation to *backends.py*
    - Delete *token.py* from the project since its functions have been moved to *backends.py*
    
    **How should this be tested?**
    - Clone this repository
    - Checkout to bg-fix-key-error-on-get-endpoint-160246870
    - Create and activate a virtual environment for this project if you have done that already
    - Install project dependencies by running `pip install -r requirements.txt`
    - Create a `.env` file and inside it set `SECRET_KEY` and `DATABASE_URL`
    - Run the project from the terminal by issuing `python manage.py runserver`
    - using a browser of your choice, navigate to `http://127.0.0.1:8000/api/users/signup/` or `http://127.0.0.1:8000/api/users/login/
    
    **Any background context?**
    Providing descriptive responses to users when they use methods or format which are not allowed is good for a better user experience.
    
    **What are the relevant pivotal tracker stories?**
    [#160246870](https://www.pivotaltracker.com/story/show/160246870)

--- authors/apps/authentication/backends.py
+++ authors/apps/authentication/backends.py
@@ -1,4 +1,5 @@
-# import jwt
+import jwt
+import datetime
 
 from django.conf import settings
 
@@ -6,8 +7,19 @@ from rest_framework import authentication, exceptions
 
 from .models import User
 
-"""Configure JWT Here"""
 
+def generate_jwt_token(username):
+    """
+    This method generates a jwt string with username encoded in it.
+    :params str username: A unique name for every user in the system
+    :returns: str JWT: A string with username name encoded in it.
+    """
+    time = datetime.datetime.utcnow() + datetime.timedelta(seconds=86400)
+    token = jwt.encode({
+        "username": username,
+        "exp": time,
+    }, settings.SECRET_KEY, algorithm='HS256')
 
+    return token.decode('utf-8')
 class JWTAuthentication(authentication.TokenAuthentication):
     pass
