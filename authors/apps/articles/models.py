e47b04450f8fbf338c23beeb3ae0661795af8ae9
Meshack Mbuvi <meshmbuvi@gmail.com>
2018-09-05 18:56:26 +0300

    [test]: add tests and models for creating new articles
    - ensure that all edge cases when creating new articles are covered.
    
    [WIP #159965306]

---
+++ authors/apps/articles/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.utils.text import slugify
+from authors.apps.authentication.models import User
+
+
+class Images(models.Model):
+    """
+    Handles images for the Authors Haven. Request to handle images should be
+    handled by other model referencing this class. No requests should be
+    send directly to this model since this model is meant to be referenced.
+    """
+    # save images as filenames in the database. Filenames should be very short
+    name = models.CharField(max_length=30)
+
+
+class Article(models.Model):
+    """
+    Defines fields for each article.
+    """
+    class Meta:
+        ordering = ['-published_at']
+
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    description = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    published_at = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(unique=True, editable=False, max_length=140)
+    favorited = models.BooleanField(default=False, null=True)
+    favoritesCount = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+    def _generate_unique_slug(self):
+        slug = slugify(self.title)
+        num = 1
+        while Article.objects.filter(slug=slug).exists():
+            unique_slug = '{}-{}'.format(slug, num)
+            num += 1
+        return unique_slug
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = self._generate_unique_slug()
+        super().save(*args, **kwargs)
---
+++ authors/apps/articles/tests/test_create_article.py
@@ -0,0 +1,57 @@
+from .base_setup import Base
+from rest_framework import status
+
+
+class ArticleTests(Base):
+    def setUp(self):
+        super().setUp()
+
+    def tearDown(self):
+        super().tearDown()
+
+    def test_create_article(self):
+        """
+        Tests a that user can create a new article
+        """
+        response = self.client.post(self.article_url, self.article_data,
+                                    format='json',
+                                    Authorization=self.authorization)
+        self.assertEqual(response.status, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['article']
+                         ['title'], self.article_data['title'])
+
+    def test_cannot_create_article_with_missing_a_title(self):
+        """
+        Tests a that user cannot create a new article without a title
+        """
+        self.article_data['title'] = None
+        response = self.client.post(self.article_url, self.article_data,
+                                    format='json',
+                                    Authorization=self.authorization)
+        self.assertEqual(response.status, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data['message'],
+                         'An article must have a title')
+
+    def test_cannot_create_article_with_missing_a_body(self):
+        """
+        Tests a that user cannot create a new article without a body
+        """
+        self.article_data['body'] = None
+        response = self.client.post(self.article_url, self.article_data,
+                                    format='json',
+                                    Authorization=self.authorization)
+        self.assertEqual(response.status, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data['message'],
+                         'An article must have a body')
+
+    def test_cannot_create_article_with_missing_a_description(self):
+        """
+        Tests a that user cannot create a new article without a description
+        """
+        self.article_data['description'] = None
+        response = self.client.post(self.article_url, self.article_data,
+                                    format='json',
+                                    Authorization=self.authorization)
+        self.assertEqual(response.status, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data['message'],
+                         'An article must have a description')
