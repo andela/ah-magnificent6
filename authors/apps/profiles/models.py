4d9e7112a1d6985a445056e0a9adeedd1d5f8a3c
Paul Gathegu <gathegu@Pauls-MacBook-Pro.local>
2018-09-06 15:32:30 +0300

    [Feature #159965305] Update Profile model

--- authors/apps/profiles/models.py
+++ authors/apps/profiles/models.py
@@ -10,22 +10,28 @@ GENDER_CHOICES = (
     ('Female', 'Female')
 )
 
-class UserProfile(models.Model):
+class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    bio = models.TextField(default='', null=True, blank=True)
-    avatar = models.ImageField(upload_to='avatars', blank=True)
-    gender = models.CharField(max_length=6, choices=GENDER_CHOICES)
-    city = models.CharField(max_length=100, default='')
-    country = models.CharField(max_length=100, default='')
-    phone_number = models.IntegerField(default=0)
-    website = models.URLField(default='', blank=True)
-    following = models.BooleanField(default=False)
+    first_name = models.CharField(_('first name'), max_length=30, blank=True)
+    last_name = models.CharField(_('last name'), max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    bio = models.TextField(_('bio'),default='', null=True, blank=True)
+    gender = models.CharField(_('gender'), max_length=6, choices=GENDER_CHOICES)
+    avatar = models.ImageField(_('avatar/'), upload_to='avatars', null=True, blank=True)
+    city = models.CharField(_('city'), max_length=100, default='')
+    country = models.CharField(_('country'), max_length=100, default='')
+    phone = models.IntegerField(_('phone'), default=0)
+    website = models.URLField(_('website'), default='', blank=True)
+    following = models.BooleanField(_('following'), default=False)
+    created_at = models.DateTimeField(_('date joined'), auto_now_add=True)
+    updated_at = models.DateTimeField(_('updated at'), auto_now_add=False, auto_now=True)
 
     def __str__(self):
         return self.user.username
 
 def create_profile(sender, **kwargs):
     if kwargs.get('created'):
-        user_profile = UserProfile.objects.create(user=kwargs.get('instance')
-
+        user_profile = Profile(user=kwargs.get('instance'))
+        user_profile.save()
+        
 post_save.connect(create_profile, sender=settings.AUTH_USER_MODEL)
