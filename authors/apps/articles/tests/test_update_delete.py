bd82f7f40ec1e36bedc37c662f4f555f07ef26fa
Meshack Mbuvi <meshmbuvi@gmail.com>
2018-09-09 21:23:19 +0300

    [feat](testing): add tests for CRUD on articles
    - Ensure to have tests for creating articles
    - Ensure to have tests for updating an article
    - Ensure to have tests for deleting an article
    - Ensure to have tests for retrieving all articles and also a single article.
    
    [WIP #159965306]

--- authors/apps/articles/tests/test_update_delete.py
+++ authors/apps/articles/tests/test_update_delete.py
@@ -3,7 +3,7 @@ from rest_framework import status
 from django.urls import reverse
 
 
-class ArticleTests(Base):
+class ArticleDeleteUpdateTests(Base):
     def setUp(self):
         super().setUp()
         self.client.post(self.article_url, self.article_data,
@@ -18,9 +18,46 @@ class ArticleTests(Base):
         """
         Tests that a client can delete a specific article
         """
-        response = self.client.get(
-            self.article_url, format="json", **self.headers)
-        print(response.data)
         response = self.client.delete(
             self.retrieve_update_delete_url, format="json", **self.headers)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_cannot_delete_other_users_articles(self):
+        """
+        Tests that a user cannot delete article they do not own
+        """
+        # Log in/ register a new user and use this user's credentials to
+        # delete article belonging to the other user
+        user_data = {
+            "username": "new_user",
+            "email": "user@gmail.com",
+            "password": "kamila1990",
+        }
+        response = self.client.post(self.registration_url, user_data,
+                                    format='json')
+        headers = {'HTTP_AUTHORIZATION': 'Bearer {}'.format(
+            response.data['token'])}
+        self.client.post(self.article_url, self.article_data,
+                         format="json", **headers)
+        response = self.client.get(
+            self.article_url, format="json", **self.headers)
+        response = self.client.delete(
+            reverse(
+                'articles:retrieveUpdateDelete', kwargs={'pk': 6}),
+            format="json",
+            **headers)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_can_edit_an_article(self):
+        """
+        Tests that a client can update details of an article
+        """
+        self.article_data['body'] = 'This is another story'
+        response = self.client.put(
+            reverse(
+                'articles:retrieveUpdateDelete', kwargs={'pk': 5}),
+            data=self.article_data,
+            format="json",
+            **self.headers)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['body'], self.article_data['body'])
