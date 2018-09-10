edb06a1d92630aee45fbbc0f3904b487261c83c5
Paul Gathegu <gathegu@Pauls-MacBook-Pro.local>
2018-09-10 15:35:42 +0300

    [Feature #159965305] Update Profile model

--- authors/apps/profiles/models.py
+++ authors/apps/profiles/models.py
@@ -5,26 +5,19 @@ from django.db.models.signals import post_save
 from authors.apps.core.models import TimeStampModel
 from authors import settings
 
-GENDER_CHOICES = (
-    ('Male', 'Male'),
-    ('Female', 'Female')
-)
-
 class Profile(TimeStampModel):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     first_name = models.CharField(_('first name'), max_length=30, blank=True)
     last_name = models.CharField(_('last name'), max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
     bio = models.TextField(_('bio'),default='', null=True, blank=True)
-    gender = models.CharField(_('gender'), max_length=6, choices=GENDER_CHOICES)
-    avatar = models.ImageField(_('avatar/'), upload_to='avatars', null=True, blank=True)
-    city = models.CharField(_('city'), max_length=100, default='')
-    country = models.CharField(_('country'), max_length=100, default='')
-    phone = models.IntegerField(_('phone'), default=0)
-    website = models.URLField(_('website'), default='', blank=True)
-    follows = models.ManyToManyField('self', related_name='followed_by', symmetrical=False)
+    avatar = models.URLField(_('avatar'), null=True, blank=True)
+    city = models.CharField(_('city'), max_length=100, blank=True, null=True, default='')
+    country = models.CharField(_('country'), max_length=100, blank=True, null=True, default='')
+    phone = models.IntegerField(_('phone'), blank=True, null=True, default=0)
+    website = models.URLField(_('website'), blank=True, null=True, default='')
+    # following = models.ManyToManyField('self', related_name='followed_by', symmetrical=False, blank=True)
     
-
     def __str__(self):
         return self.user.username
 
