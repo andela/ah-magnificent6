30000c10b270409f8fbf8c97bffefa7c3d521886
Michael Nthiwa <mike.nthiwa@gmail.com>
2018-09-11 09:42:49 +0300

    [feature #159965303] Rename the endpoint forget_password to forgot_password

--- authors/apps/authentication/urls.py
+++ authors/apps/authentication/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
          name='current_user'),
     path('users/signup/', RegistrationAPIView.as_view(), name='register'),
     path('users/login/', LoginAPIView.as_view(), name='login'),
-    path('accounts/forget_password/', ForgotPasswordAPIView.as_view(), name='forgot'),
+    path('accounts/forgot_password/', ForgotPasswordAPIView.as_view(), name='forgot'),
     path('reset_password/<str:token>/', ResetPasswordAPIView.as_view(), name='reset_password')
 ]
