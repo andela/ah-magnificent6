28c648984c08fbfcfaf487b7a758b449939f20af
Paul Gathegu <13488744+pngg@users.noreply.github.com>
2018-09-12 15:50:08 +0300

    #159965305 Create User Profiles (#20)
    #### What does this PR do?
    Create user profiles
    
    #### Description of Task to be completed
    - Create a user profile when a user is created

--- authors/apps/authentication/urls.py
+++ authors/apps/authentication/urls.py
@@ -8,7 +8,7 @@ from .views import (
 app_name = 'authentication'
 
 urlpatterns = [
-    path('user/<int:user_id>', UserRetrieveUpdateAPIView.as_view(),
+    path('user', UserRetrieveUpdateAPIView.as_view(),
          name='current_user'),
     path('users/signup/', RegistrationAPIView.as_view(), name='register'),
     path('users/login/', LoginAPIView.as_view(), name='login'),
