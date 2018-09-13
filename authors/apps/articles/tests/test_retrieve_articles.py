f8a8e3e23d1633c81574b99883b52f1fc969ac5b
PAbishai <abishai.omari@andela.com>
2018-09-13 15:29:09 +0300

    [Feature #159965316] Add pagination support

--- authors/apps/articles/tests/test_retrieve_articles.py
+++ authors/apps/articles/tests/test_retrieve_articles.py
@@ -8,7 +8,8 @@ class ArticleTests(Base):
         super().setUp()
         response = self.client.get(
             self.article_url, format="json", **self.headers)
-        self.initial_count = len(response.data)
+        print(response)
+        self.initial_count = len(response.data["results"])
         response = self.client.post(self.article_url, self.article_data,
                                     format="json", **self.headers)
         self.article_id = response.data['id']
@@ -25,8 +26,9 @@ class ArticleTests(Base):
         """
         response = self.client.get(
             self.article_url, format="json", **self.headers)
+        print(response.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertTrue(len(response.data) > self.initial_count)
+        self.assertTrue(len(response.data["results"]) > self.initial_count)
 
     def test_can_retrieve_a_single_article(self):
         """
