c307c215dad6490449e983f0e00ac42876b9e2bd
Paul Gathegu <gathegu@Pauls-MacBook-Pro.local>
2018-09-18 11:03:55 +0300

    [Feature 159965318] Add time_to_read test

---
+++ authors/apps/articles/tests/test_time_to_read.py
@@ -0,0 +1,26 @@
+from .base_setup import Base
+from rest_framework import status
+from django.urls import reverse
+
+
+class ArticleReadTime(Base):
+    def setUp(self):
+        super().setUp()
+        response = self.client.get(
+            self.article_url, format="json", **self.headers)
+        self.initial_count = len(response.data["results"])
+        response = self.client.post(self.article_url, self.article_data,
+                                    format="json", **self.headers)
+        self.article_slug = response.data['slug']
+        self.time_to_read = '0 min'
+        self.retrieve_update_delete_url = reverse(
+            'articles:retrieveUpdateDelete', kwargs={'slug': self.article_slug})
+
+    def test_can_retrieve_article_with_read_time(self):
+        """
+        Tests that a client can retrieve an article with the read time 
+        """
+        response = self.client.get(self.retrieve_update_delete_url,
+                                   format="json", **self.headers)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(response.data['time_to_read'] == self.time_to_read)
