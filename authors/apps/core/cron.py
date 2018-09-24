7a763406c1d9e00315b6f14b4d41669727939310
Bileonaire <leonkioko@Leons-MacBook-Pro.local>
2018-09-24 16:39:21 +0300

    [Feature #159965313] Fix naming ambiguity

--- authors/apps/core/cron.py
+++ authors/apps/core/cron.py
@@ -18,20 +18,20 @@ class EmailNotificationCron(CronJobBase):
         """Send emails to all the persons to be notified."""
         subject = 'Authors Haven Notification'
 
-        emails = []
+        receipients = []
         notifications = Notification.objects.all()
         for notification in notifications:
             if notification.email_sent == False:
                 for user in notification.notified.all():
                     if user not in notification.read.all():
                         if user.profile.email_notification_enabled == True:
-                            emails.append(user.email)
+                            receipients.append(user.email)
                 content = {'notification': notification}
                 message = render_to_string("notification.html", content)
                 mail = EmailMessage(
                     subject=subject,
                     body=message,
-                    to=emails,
+                    to=receipients,
                     from_email=settings.EMAIL_HOST_USER)
                 mail.content_subtype = "html"
                 mail.send(fail_silently=False)
