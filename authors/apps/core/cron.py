38e8032188d43ef40ed0309342a36f790588c836
Bileonaire <leonkioko@Leons-MacBook-Pro.local>
2018-09-24 22:26:06 +0300

    [Feature #159965313] Fix typo

--- authors/apps/core/cron.py
+++ authors/apps/core/cron.py
@@ -18,20 +18,20 @@ class EmailNotificationCron(CronJobBase):
         """Send emails to all the persons to be notified."""
         subject = 'Authors Haven Notification'
 
-        receipients = []
+        recipients = []
         notifications = Notification.objects.all()
         for notification in notifications:
             if notification.email_sent == False:
                 for user in notification.notified.all():
                     if user not in notification.read.all():
                         if user.profile.email_notification_enabled == True:
-                            receipients.append(user.email)
+                            recipients.append(user.email)
                 content = {'notification': notification}
                 message = render_to_string("notification.html", content)
                 mail = EmailMessage(
                     subject=subject,
                     body=message,
-                    to=receipients,
+                    to=recipients,
                     from_email=settings.EMAIL_HOST_USER)
                 mail.content_subtype = "html"
                 mail.send(fail_silently=False)
